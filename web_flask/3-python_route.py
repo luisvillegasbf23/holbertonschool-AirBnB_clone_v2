f782b07fcb51781dcb855a3800db097718082fe8
luisvillegasbf23 <luisville.lv@gmail.com>
2022-11-29 23:02:33 -0300

    feat: task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""module with hello world"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ first end point """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ second  end point """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """capture variable"""
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python/<text>')
+def python(text):
+    """capture variable python"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
